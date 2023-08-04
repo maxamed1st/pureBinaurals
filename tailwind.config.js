30f4f0fe7fb73d425da684ec8ca7ce4829bacec2
maxamed1st <maxamed1st@icloud.com>
2023-08-04 20:52:04 +0100

    add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -12,6 +14,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 }
-
