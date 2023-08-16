82d4dcb5a2a66f02ac5dc3ff159899c0af352fe2
maxamed1st <maxamed1st@icloud.com>
2023-08-16 12:59:25 +0100

    set up dashboard with regards to the diagram

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,9 +1,15 @@
-import Create from "./Create"
+import Playlist from "./PlayList"
+import Create from "./Create";
+import Player from "./Player"
 
 export default function Dashboard() {
+  //create context
+  const ctx = new AudioContext();
   return (
     <main>
       <Create />
+      <Playlist />
+      <Player ctx={ctx} />
     </main>
   )
 }
