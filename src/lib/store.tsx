978bf483c8894f23f11e8f6b82d820911505cbcc
maxamed1st <maxamed1st@icloud.com>
2023-08-17 15:57:24 +0100

    create playList store

---
+++ src/lib/store.tsx
@@ -0,0 +1,39 @@
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface Beat {
+  id: number;
+  title: string;
+  frequency: number;
+  duration: number;
+}
+
+const playListSlice = createSlice({
+  name: 'playList',
+  initialState: [] as Beat[],
+  reducers: {
+    addBeat: (state, action: PayloadAction<Beat>) => {
+      state.push(action.payload);
+    },
+    updateBeat: (state, action: PayloadAction<{ id: number; updatedBeat: Partial<Beat> }>) => {
+      const { id, updatedBeat } = action.payload;
+      const existingBeat = state.find(beat => beat.id === id);
+      if (existingBeat) {
+        Object.assign(existingBeat, updatedBeat);
+      }
+    },
+    deleteBeat: (state, action: PayloadAction<number>) => {
+      const beatId = action.payload;
+      return state.filter(beat => beat.id !== beatId);
+    },
+  },
+});
+
+export const { addBeat, updateBeat, deleteBeat } = playListSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    playList: playListSlice.reducer,
+  },
+});
+
+export default store;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global.css'
+import { Provider } from 'react-redux'
+import store from './lib/store.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>
 )
